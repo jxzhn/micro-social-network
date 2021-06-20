c63c2516ba9985f54971ac1b9fd2a1d6c840eeee
ChaCha-Chan <2507788320@qq.com>
2021-06-20 23:18:47 +0800

    更新js

--- js/login.js
+++ js/login.js
@@ -1,10 +1,23 @@
 var username = document.getElementById("username");
 var password = document.getElementById("password");
 var errormsg = document.getElementById("errormsg");
+
 function login() {
     //validate
-    var error_flag = true;
+    var error_flag = validate(username.value, password.value);
     if(error_flag){
         errormsg.innerHTML = "用户名或密码错误";
+    }
+    else{
+        errormsg.innerHTML = "登陆成功";
+    }
+}
+
+function validate(username, password) {
+    if(username == password){
+        return true;
+    }
+    else{
+        return false;
     }
 }
