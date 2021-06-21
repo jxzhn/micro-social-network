9a813dbeeb8f3238d3bee57dcee5cac9e648156a
jxzhn <jxzhn@jxzhn.com>
2021-06-21 16:37:01 +0800

    ajax

--- js/ajax.js
+++ js/ajax.js
@@ -1,13 +1,21 @@
-async function postJsonRequest(url, data) {
+async function callCloudMethod(url, data) {
     return new Promise((resolve, reject) => {
         var xmlhttp = new XMLHttpRequest();
         xmlhttp.open('post', url, true);
         xmlhttp.setRequestHeader('content-type', 'application/json');
         xmlhttp.onload = () => {
             if (xmlhttp.getResponseHeader('content-type').toLowerCase().includes('application/json')) {
-                resolve(JSON.parse(xmlhttp.responseText))
+                resp = JSON.parse(xmlhttp.responseText);
+                if (resp.code == 0) {
+                    resolve(resp.data);
+                } else if (resp.code == 999) { // 未登录
+                    window.location.href = '/login.html';
+                    reject('未登录');
+                } else {
+                    reject(resp.msg);
+                }
             } else {
-                reject(xmlhttp);
+                reject('未知: ' + xmlhttp.responseText);
             }
         }
         xmlhttp.send(JSON.stringify(data));
