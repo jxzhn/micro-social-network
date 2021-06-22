2dae6c158c289d1964134c8673d48253d6c4e83f
ChaCha-Chan <2507788320@qq.com>
2021-06-22 23:29:07 +0800

    修正变量名错误

--- js/login.js
+++ js/login.js
@@ -4,14 +4,11 @@ var errormsg = document.getElementById("errormsg");
 
 async function login() {
     //validate
-    var loginInfo = {
-        id: userId.value,
-        password: password.value
-    }
+   
     var res = await ajax.post("/user/login", loginInfo);
     console.log(res);
     if(res.code != 0){
-        errormsg.innerHTML = res.message;
+        errormsg.innerHTML = res.msg;
     }
     else{
         window.location.href = "./index.html";
