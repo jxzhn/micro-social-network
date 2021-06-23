3fd0d10b7f8e6f07862a3ef9d42e967d43dbfdd4
ChaCha-Chan <2507788320@qq.com>
2021-06-23 19:55:40 +0800

    修正点赞和时间显示

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+var TEST_FLAG = true;
+
 var loading = document.getElementById('loading');
 var loadingLock = false;
 var loadedTweetList = [];
@@ -34,7 +36,7 @@ function showTweets(tweetList) {
                 `<span class="tweet-user-name" onclick="goUserProfile(${i})">${tweet.user.userName}</span>\n` + 
                 `<span class="tweet-user-id" onclick="goUserProfile(${i})">@${tweet.user.userId}</span>\n` + 
                 `<span class="tweet-dot">.</span>\n` + 
-                `<span class="tweet-date">${new Date(tweet.date).Format('MM 月 dd 日')}</span>\n` + 
+                `<span class="tweet-date">${new Date(tweet.date*1000).Format('MM 月 dd 日')}</span>\n` + 
             `</div>\n` + 
             `<div class="tweet-content" onclick="goDetail(${i})">\n` + 
                 `<span>${tweet.content}</span>\n` + 
@@ -49,6 +51,81 @@ function showTweets(tweetList) {
         loading.parentNode.insertBefore(block, loading);
     }
 }
+var LOAD_FLAG = true;
+var tweetListTest = {
+    "posts" : [
+        {
+            postId: Math.round(Math.random() * 1000000000),
+            user: {
+                userName: "Yes Theory",
+                userId: "YesTheory1",
+                userImgUrl: "https://avatars.githubusercontent.com/u/84268956?v=4"
+            },
+            date: Math.round(new Date().getTime() / 1000),
+            content: "Don't wait for the opportunity of an adventure to present itself to you. Go seek it for yourself wherever you are ⚡⚡⚡",
+            imgUrl: "https://pic2.zhimg.com/50/v2-a8971875ffbcabefe0eb4bc9f478d126_hd.jpg?source=1940ef5c",
+            numComment: 20,
+            liked: true,
+            numLike: 143
+        },
+        {
+            postId: Math.round(Math.random() * 1000000000),
+            user: {
+                userName: "Yes Theory",
+                userId: "YesTheory1",
+                userImgUrl: "https://avatars.githubusercontent.com/u/84268956?v=4"
+            },
+            date:  Math.round(new Date().getTime() / 1000),
+            content: "Don't wait for the opportunity of an adventure to present itself to you. Go seek it for yourself wherever you are ⚡⚡⚡",
+            imgUrl: "",
+            numComment: 20,
+            liked: true,
+            numLike: 143
+        },
+        {
+            postId: Math.round(Math.random() * 1000000000),
+            user: {
+                userName: "Yes Theory",
+                userId: "YesTheory2",
+                userImgUrl: "https://avatars.githubusercontent.com/u/84268956?v=4"
+            },
+            date:  Math.round(new Date().getTime() / 1000),
+            content: "Don't wait for the opportunity of an adventure to present itself to you. Go seek it for yourself wherever you are ⚡⚡⚡",
+            imgUrl: "https://pic2.zhimg.com/50/v2-a8971875ffbcabefe0eb4bc9f478d126_hd.jpg?source=1940ef5c",
+            numComment: 20,
+            liked: true,
+            numLike: 143
+        },
+        {
+            postId: Math.round(Math.random() * 1000000000),
+            user: {
+                userName: "Yes Theory",
+                userId: "YesTheory3",
+                userImgUrl: "https://avatars.githubusercontent.com/u/84268956?v=4"
+            },
+            date:  Math.round(new Date().getTime() / 1000),
+            content: "Don't wait for the opportunity of an adventure to present itself to you. Go seek it for yourself wherever you are ⚡⚡⚡",
+            imgUrl: "https://pic2.zhimg.com/50/v2-a8971875ffbcabefe0eb4bc9f478d126_hd.jpg?source=1940ef5c",
+            numComment: 20,
+            liked: true,
+            numLike: 143
+        },
+        {
+            postId: Math.round(Math.random() * 1000000000),
+            user: {
+                userName: "Yes Theory",
+                userId: "YesTheory4",
+                userImgUrl: "https://avatars.githubusercontent.com/u/84268956?v=4"
+            },
+            date:  Math.round(new Date().getTime() / 1000),
+            content: "Don't wait for the opportunity of an adventure to present itself to you. Go seek it for yourself wherever you are ⚡⚡⚡",
+            imgUrl: "https://pic2.zhimg.com/50/v2-a8971875ffbcabefe0eb4bc9f478d126_hd.jpg?source=1940ef5c",
+            numComment: 20,
+            liked: true,
+            numLike: 143
+        }
+    ]
+}
 
 async function loadMoreTweets(numTweet) {
     if (loadingLock) {
@@ -57,68 +134,69 @@ async function loadMoreTweets(numTweet) {
     loadingLock = true;
     loading.style.display = 'block';
     // 用 AJAX 向服务器请求 numTweet 条数据，这里先弄点假数据
-    var tweetList = await new Promise((resolve, reject) => {
-        setTimeout(() => {
-            tweetList = [
-                {
-                    id: Math.round(Math.random() * 1000000000),
-                    user: {
-                        userName: "一位路过的靓仔",
-                        userId: "handsomeboy",
-                        userImgUrl: "https://avatars.githubusercontent.com/u/84268960?v=4"
-                    },
-                    date: new Date().toLocaleDateString(),
-                    content: "新买的ThinkPad，刚刚开封，系统自带win10，没有安装其他任何第三方软件。第一个安装的是搜狗输入法，刚装上就发了个弹窗：检测到系统存在9个垃圾软件，建议清理巴拉巴拉。嗯，系统里除了你，我还没有安装任何其他东西呢，你到还是真直觉，这么快就把自己归入垃圾软件了",
-                    imgUrl: "",
-                    numComment: 8,
-                    liked: false,
-                    numLike: 20 
-                },
-                {
-                    id: Math.round(Math.random() * 1000000000),
-                    user: {
-                        userName: "Yes Theory",
-                        userId: "YesTheory",
-                        userImgUrl: "https://avatars.githubusercontent.com/u/84268956?v=4"
-                    },
-                    date: new Date(new Date - 24*3600*1000).toLocaleDateString(),
-                    content: "Don't wait for the opportunity of an adventure to present itself to you. Go seek it for yourself wherever you are ⚡⚡⚡",
-                    imgUrl: "https://pic2.zhimg.com/50/v2-a8971875ffbcabefe0eb4bc9f478d126_hd.jpg?source=1940ef5c",
-                    numComment: 20,
-                    liked: true,
-                    numLike: 143
-                }
-            ];
-            while (tweetList.length < numTweet) {
-                tweetList.push(...tweetList);
-            }
-            resolve(tweetList.slice(0, numTweet));
-        }, 1000);
-    });
+    var tweetList = [];
+    try{
+        var infoToSend = {
+            timeStamp: Math.round(new Date().getTime() / 1000),
+            loadedNum: loadedTweetList.length,
+            requestNum: numTweet
+        }
+        console.log("send to /post/list/follow:");
+        console.log(infoToSend);
+        if(TEST_FLAG) tweetList = tweetListTest.posts;
+        else tweetList = (await ajax.post("/post/list/follow", infoToSend)).posts;
+    }
+    catch(err){
+        console.log(err);
+    }
+
+    if(tweetList.length < numTweet){
+        LOAD_FLAG = false;
+    }
     loadedTweetList.push(...tweetList);
     showTweets(tweetList);
     loading.style.display = 'none';
     loadingLock = false;
 }
 
-function clickLike(likeElement, i) {
-    console.log(i);
+async function clickLike(likeElement, i) {
     var tweet = loadedTweetList[i];
-
-    // 发送 AJAX
-
+    var likeInfo = {
+        postId: loadedTweetList[i].postId
+    }
+    if(tweet.liked){
+        try{
+            console.log("send to /post/dislike:");
+            console.log(likeInfo);
+            if(!TEST_FLAG)await ajax.post("post/dislike", likeInfo);
+        }
+        catch(err){
+            console.log(err);
+        }
+    }
+    else{
+        try{
+            console.log("send to /post/like:");
+            console.log(likeInfo);
+            if(!TEST_FLAG)await ajax.post("/post/like", likeInfo);
+        }
+        catch(err){
+            console.log(err);
+        }
+    }
     tweet.liked = !tweet.liked;
     likeElement.classList = `tweet-like ${tweet.liked?'tweet-liked':''}`;
     likeElement.childNodes[0].classList = `${tweet.liked?'fas':'far'} fa-heart`;
 }
 
 function goDetail(i) {
-    window.location.href = "/detail.html?id=" + loadedTweetList[i].id;
+    window.location.href = "./detail.html?postId=" + loadedTweetList[i].postId;
 }
 
 function goUserProfile(i) {
-    window.location.href = "/profile.html?id=" + loadedTweetList[i].user.userId;
+    window.location.href = "./profile.html?id=" + loadedTweetList[i].user.userId;
 }
+
 
 window.addEventListener('scroll', () => {
     // 变量scrollTop是滚动条滚动时，离顶部的距离
@@ -130,7 +208,7 @@ window.addEventListener('scroll', () => {
     // 判断滚动条是否到底部
     if(scrollTop + windowHeight >= scrollHeight - 10){
         //写后台加载数据的函数
-        loadMoreTweets(5);
+        if(LOAD_FLAG)loadMoreTweets(6);
     }
 })
 
--- js/profile.js
+++ js/profile.js
@@ -295,7 +295,7 @@ function showTweets(tweetList) {
                 `<span class="tweet-user-name" onclick="goUserProfile(${i})">${tweet.user.userName}</span>\n` + 
                 `<span class="tweet-user-id" onclick="goUserProfile(${i})">@${tweet.user.userId}</span>\n` + 
                 `<span class="tweet-dot">.</span>\n` + 
-                `<span class="tweet-date">${new Date(tweet.date).Format('MM 月 dd 日')}</span>\n` + 
+                `<span class="tweet-date">${new Date(tweet.date*1000).Format('MM 月 dd 日')}</span>\n` + 
             `</div>\n` + 
             `<div class="tweet-content" onclick="goDetail(${i})">\n` + 
                 `<span>${tweet.content}</span>\n` + 
@@ -320,7 +320,7 @@ var tweetListTest = {
                 userId: "YesTheory1",
                 userImgUrl: "https://avatars.githubusercontent.com/u/84268956?v=4"
             },
-            date: new Date(new Date - 24*3600*1000).toLocaleDateString(),
+            date: Math.round(new Date().getTime() / 1000),
             content: "Don't wait for the opportunity of an adventure to present itself to you. Go seek it for yourself wherever you are ⚡⚡⚡",
             imgUrl: "https://pic2.zhimg.com/50/v2-a8971875ffbcabefe0eb4bc9f478d126_hd.jpg?source=1940ef5c",
             numComment: 20,
@@ -334,7 +334,7 @@ var tweetListTest = {
                 userId: "YesTheory1",
                 userImgUrl: "https://avatars.githubusercontent.com/u/84268956?v=4"
             },
-            date: new Date(new Date - 24*3600*1000).toLocaleDateString(),
+            date:  Math.round(new Date().getTime() / 1000),
             content: "Don't wait for the opportunity of an adventure to present itself to you. Go seek it for yourself wherever you are ⚡⚡⚡",
             imgUrl: "",
             numComment: 20,
@@ -348,7 +348,7 @@ var tweetListTest = {
                 userId: "YesTheory2",
                 userImgUrl: "https://avatars.githubusercontent.com/u/84268956?v=4"
             },
-            date: new Date(new Date - 24*3600*1000).toLocaleDateString(),
+            date:  Math.round(new Date().getTime() / 1000),
             content: "Don't wait for the opportunity of an adventure to present itself to you. Go seek it for yourself wherever you are ⚡⚡⚡",
             imgUrl: "https://pic2.zhimg.com/50/v2-a8971875ffbcabefe0eb4bc9f478d126_hd.jpg?source=1940ef5c",
             numComment: 20,
@@ -362,7 +362,7 @@ var tweetListTest = {
                 userId: "YesTheory3",
                 userImgUrl: "https://avatars.githubusercontent.com/u/84268956?v=4"
             },
-            date: new Date(new Date - 24*3600*1000).toLocaleDateString(),
+            date:  Math.round(new Date().getTime() / 1000),
             content: "Don't wait for the opportunity of an adventure to present itself to you. Go seek it for yourself wherever you are ⚡⚡⚡",
             imgUrl: "https://pic2.zhimg.com/50/v2-a8971875ffbcabefe0eb4bc9f478d126_hd.jpg?source=1940ef5c",
             numComment: 20,
@@ -376,7 +376,7 @@ var tweetListTest = {
                 userId: "YesTheory4",
                 userImgUrl: "https://avatars.githubusercontent.com/u/84268956?v=4"
             },
-            date: new Date(new Date - 24*3600*1000).toLocaleDateString(),
+            date:  Math.round(new Date().getTime() / 1000),
             content: "Don't wait for the opportunity of an adventure to present itself to you. Go seek it for yourself wherever you are ⚡⚡⚡",
             imgUrl: "https://pic2.zhimg.com/50/v2-a8971875ffbcabefe0eb4bc9f478d126_hd.jpg?source=1940ef5c",
             numComment: 20,
@@ -422,13 +422,13 @@ async function loadMoreTweets(numTweet) {
 async function clickLike(likeElement, i) {
     var tweet = loadedTweetList[i];
     var likeInfo = {
-        postId: loadedTweetList[i].id
+        postId: loadedTweetList[i].postId
     }
     if(tweet.liked){
         try{
-            console.log("send to /user/dislike:");
+            console.log("send to /post/dislike:");
             console.log(likeInfo);
-            if(!TEST_FLAG)await ajax.post("/user/dislike", likeInfo);
+            if(!TEST_FLAG)await ajax.post("/post/dislike", likeInfo);
         }
         catch(err){
             console.log(err);
@@ -436,9 +436,9 @@ async function clickLike(likeElement, i) {
     }
     else{
         try{
-            console.log("send to /user/like:");
+            console.log("send to /post/like:");
             console.log(likeInfo);
-            if(!TEST_FLAG)await ajax.post("/user/like", likeInfo);
+            if(!TEST_FLAG)await ajax.post("/post/like", likeInfo);
         }
         catch(err){
             console.log(err);
