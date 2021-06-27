83ce3e15affea8fecfc622c88c829abc5478147c
ChaCha-Chan <2507788320@qq.com>
2021-06-27 21:50:06 +0800

    删除假数据

--- js/index.js
+++ js/index.js
@@ -1,5 +1,3 @@
-var TEST_FLAG = false;
-
 var loading = document.getElementById('loading');
 var loadingLock = false;
 var loadedTweetList = [];
@@ -52,81 +50,6 @@ function showTweets(tweetList) {
     }
 }
 var LOAD_FLAG = true;
-var tweetListTest = {
-    "posts" : [
-        {
-            postId: Math.round(Math.random() * 1000000000),
-            user: {
-                userName: "Yes Theory",
-                userId: "YesTheory1",
-                userImgUrl: "https://avatars.githubusercontent.com/u/84268956?v=4"
-            },
-            date: Math.round(new Date().getTime() / 1000),
-            content: "Don't wait for the opportunity of an adventure to present itself to you. Go seek it for yourself wherever you are ⚡⚡⚡",
-            imgUrl: "https://pic2.zhimg.com/50/v2-a8971875ffbcabefe0eb4bc9f478d126_hd.jpg?source=1940ef5c",
-            numComment: 20,
-            liked: true,
-            numLike: 143
-        },
-        {
-            postId: Math.round(Math.random() * 1000000000),
-            user: {
-                userName: "Yes Theory",
-                userId: "YesTheory1",
-                userImgUrl: "https://avatars.githubusercontent.com/u/84268956?v=4"
-            },
-            date:  Math.round(new Date().getTime() / 1000),
-            content: "Don't wait for the opportunity of an adventure to present itself to you. Go seek it for yourself wherever you are ⚡⚡⚡",
-            imgUrl: "",
-            numComment: 20,
-            liked: true,
-            numLike: 143
-        },
-        {
-            postId: Math.round(Math.random() * 1000000000),
-            user: {
-                userName: "Yes Theory",
-                userId: "YesTheory2",
-                userImgUrl: "https://avatars.githubusercontent.com/u/84268956?v=4"
-            },
-            date:  Math.round(new Date().getTime() / 1000),
-            content: "Don't wait for the opportunity of an adventure to present itself to you. Go seek it for yourself wherever you are ⚡⚡⚡",
-            imgUrl: "https://pic2.zhimg.com/50/v2-a8971875ffbcabefe0eb4bc9f478d126_hd.jpg?source=1940ef5c",
-            numComment: 20,
-            liked: true,
-            numLike: 143
-        },
-        {
-            postId: Math.round(Math.random() * 1000000000),
-            user: {
-                userName: "Yes Theory",
-                userId: "YesTheory3",
-                userImgUrl: "https://avatars.githubusercontent.com/u/84268956?v=4"
-            },
-            date:  Math.round(new Date().getTime() / 1000),
-            content: "Don't wait for the opportunity of an adventure to present itself to you. Go seek it for yourself wherever you are ⚡⚡⚡",
-            imgUrl: "https://pic2.zhimg.com/50/v2-a8971875ffbcabefe0eb4bc9f478d126_hd.jpg?source=1940ef5c",
-            numComment: 20,
-            liked: true,
-            numLike: 143
-        },
-        {
-            postId: Math.round(Math.random() * 1000000000),
-            user: {
-                userName: "Yes Theory",
-                userId: "YesTheory4",
-                userImgUrl: "https://avatars.githubusercontent.com/u/84268956?v=4"
-            },
-            date:  Math.round(new Date().getTime() / 1000),
-            content: "Don't wait for the opportunity of an adventure to present itself to you. Go seek it for yourself wherever you are ⚡⚡⚡",
-            imgUrl: "https://pic2.zhimg.com/50/v2-a8971875ffbcabefe0eb4bc9f478d126_hd.jpg?source=1940ef5c",
-            numComment: 20,
-            liked: true,
-            numLike: 143
-        }
-    ]
-}
-
 async function loadMoreTweets(numTweet) {
     if (loadingLock) {
         return;
@@ -143,8 +66,7 @@ async function loadMoreTweets(numTweet) {
         }
         console.log("send to /post/list/follow:");
         console.log(infoToSend);
-        if(TEST_FLAG) tweetList = tweetListTest.posts;
-        else tweetList = (await ajax.post("/post/list/follow", infoToSend)).posts;
+        tweetList = (await ajax.post("/post/list/follow", infoToSend)).posts;
     }
     catch(err){
         console.log(err);
@@ -169,7 +91,7 @@ async function clickLike(likeElement, i) {
         try{
             console.log("send to /post/dislike:");
             console.log(likeInfo);
-            if(!TEST_FLAG)await ajax.post("/post/dislike", likeInfo);
+            await ajax.post("/post/dislike", likeInfo);
             likeElement.lastChild.nodeValue = parseInt(likeElement.lastChild.nodeValue) - 1;
         }
         catch(err){
@@ -180,7 +102,7 @@ async function clickLike(likeElement, i) {
         try{
             console.log("send to /post/like:");
             console.log(likeInfo);
-            if(!TEST_FLAG)await ajax.post("/post/like", likeInfo);
+            await ajax.post("/post/like", likeInfo);
             likeElement.lastChild.nodeValue = parseInt(likeElement.lastChild.nodeValue) + 1;
         }
         catch(err){
